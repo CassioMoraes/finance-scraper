8bb3e4c65040cea60306b7d48078775044e3deb5
Cassio Castilhos Moraes <cassio.moraes@eldorado.org.br>
2019-02-08 15:18:01 -0200

    Removed not used requirements

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const router = express.Router();
 
 const stockRoute = require('./stock/stock-route');
 
--- src/stock/stock-mapper.spec.js
+++ src/stock/stock-mapper.spec.js
@@ -1,5 +1,4 @@
 const _ = require('lodash');
-const sinon = require('sinon');
 
 const StockMapper = require('./stock-mapper');
 
